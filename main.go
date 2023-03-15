c46d45e77e460bfce1553db045bf4c5530148a94
Connor <connornusser@gmail.com>
2023-03-15 02:08:48 -0500

    more go code

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 )
 
 func sayGreeting(n string) {
-	fmt.Println(n)
+	fmt.Printf("Hey welcome to narnia %v \n", n)
+}
+func cycleNames(n []string, f func(string)) {
+	for _, value := range n {
+		f(value)
+	}
+
 }
 func main() {
 	sayGreeting("hello")
+
+	strings := []string{"Hello", "How", "Are", "You"}
+	cycleNames(strings, sayGreeting)
 
 }
