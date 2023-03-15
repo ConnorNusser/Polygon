a5d6a7c6553685960df88c35bd3202105115f4bc
Connor <connornusser@gmail.com>
2023-03-15 02:04:15 -0500

    ew

--- main.go
+++ main.go
@@ -2,36 +2,12 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
+func sayGreeting(n string) {
+	fmt.Println(n)
+}
 func main() {
-	/* var nameThree string
-	fmt.Println("HelloTest")
+	sayGreeting("hello")
 
-	var nameOne string = "mario"
-	var nameTwo = "luigi"
-	fmt.Println(nameOne)
-	fmt.Println(nameTwo)
-
-	nameFour := "yoshi" */
-
-	var ageOne int = 20
-	var ageTwo = 30
-	ageThree := 40
-	fmt.Println(ageOne, ageTwo, ageThree)
-
-	//bits & memory
-	var ages = []int{20, 25, 30}
-	fmt.Println(ages[0])
-	ages = append(ages, 85)
-
-	rangeOne := ages[1:3]
-	fmt.Println(rangeOne)
-
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 30)
-	fmt.Println(index)
 }
