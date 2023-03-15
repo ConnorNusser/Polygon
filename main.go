aedb87a4b72a502c5a015e7994055f80c233369d
Connor <connornusser@gmail.com>
2023-03-14 22:32:24 -0500

    goPractice

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* var nameThree string
+	fmt.Println("HelloTest")
+
+	var nameOne string = "mario"
+	var nameTwo = "luigi"
+	fmt.Println(nameOne)
+	fmt.Println(nameTwo)
+
+	nameFour := "yoshi" */
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	//bits & memory
+}
